926ec60b22cd68302761387c73086138a7bea47e
Rem Simiyu <remsimiyu@gmail.com>
2025-03-27 10:41:25 +0300

    Investment Calculator

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,12 +1,10 @@
 import logo from '../../assets/investment-calculator-logo.png';
 
-const Header = () => {
+export default function Header() {
   return (
-    <header id='header'>
-        <img src={logo} alt="logo" />
-        <h1>Investment Calculator</h1>
+    <header id="header">
+      <img src={logo} alt="Logo showing a money bag" />
+      <h1>Investment Calculator</h1>
     </header>
-  )
+  );
 }
-
-export default Header
--- src/Components/Results/Results.jsx
+++ src/Components/Results/Results.jsx
@@ -1,48 +1,49 @@
-import React from 'react'
-import { calculateInvestmentResults, formatter } from '../../util/investment.js'
+import { calculateInvestmentResults, formatter } from '../../util/investment.js';
 
-const Results = ({ input }) => {
-const resultsData = calculateInvestmentResults(input);
 
-const initialInvestment = 
-    resultsData[0].valueEndOfYear - 
-    resultsData[0].interest - 
-    resultsData[0].annualInvestment;
+export default function Results({ input }) {
+  const results = [];
+  calculateInvestmentResults(input, results);
+
+  if (results.length === 0) {
+    return <p className="center">Invalid input data provided.</p>;
+  }
+
+  const initialInvestment =
+    results[0].valueEndOfYear -
+    results[0].interest -
+    results[0].annualInvestment;
 
   return (
-    <table id='result'>
-        <thead>
-            <tr>
-                <th>Year</th>
-                <th>Investment Value</th>
-                <th>Interest (Year)</th>
-                <th>Total Interest</th>
-                <th>Invested Capital</th>
+    <table id="result">
+      <thead>
+        <tr>
+          <th>Year</th>
+          <th>Investment Value</th>
+          <th>Interest (Year)</th>
+          <th>Total Interest</th>
+          <th>Invested Capital</th>
+        </tr>
+      </thead>
+      <tbody>
+        {results.map((yearData) => {
+          const totalInterest =
+            yearData.valueEndOfYear -
+            yearData.annualInvestment * yearData.year -
+            initialInvestment;
+          const totalAmountInvested = yearData.valueEndOfYear - totalInterest;
+
+          return (
+            <tr key={yearData.year}>
+              <td>{yearData.year}</td>
+              <td>{formatter.format(yearData.valueEndOfYear)}</td>
+              <td>{formatter.format(yearData.interest)}</td>
+              <td>{formatter.format(totalInterest)}</td>
+              <td>{formatter.format(totalAmountInvested)}</td>
             </tr>
-        </thead>
-        <tbody>
-            {resultsData.map(yearData => {
-                const totalInterest = 
-                    yearData.valueEndOfYear - 
-                    yearData.annualInvestment * yearData.year - 
-                    initialInvestment;
-
-                const totalAmountInvested = 
-                    yearData.valueEndOfYear - totalInterest;
-
-                return (
-                    <tr key={yearData.year}>
-                        <td>{yearData.year}</td>
-                        <td>{formatter.format(yearData.valueEndOfYear)}</td>
-                        <td>{formatter.format(yearData.interest)}</td>
-                        <td>{formatter.format(totalInterest)}</td>
-                        <td>{formatter.format(totalAmountInvested)}</td>
-                    </tr>
-                )
-            })}
-        </tbody>
+          );
+        })}
+      </tbody>
     </table>
-  )
+  );
 }
-
-export default Results
